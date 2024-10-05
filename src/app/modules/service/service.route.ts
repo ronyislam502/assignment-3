bd493a23605bd524684ef1583a49ed46eb4d7ce9
ronyislam502 <ronyislam502@gmail.com>
2024-10-06 03:32:10 +0600

    final

--- src/app/modules/service/service.route.ts
+++ src/app/modules/service/service.route.ts
@@ -3,8 +3,6 @@ import { ServiceControllers } from "./service.controller";
 import { SlotControllers } from "../slot/slot.controller";
 import auth from "../../middlewares/auth";
 import { USER_ROLE } from "../user/user.const";
-// import auth from "../../middlewares/auth";
-// import { USER_ROLE } from "../user/user.const";
 
 const router = express.Router();
 
@@ -12,11 +10,11 @@ router.post("/", auth(USER_ROLE.admin), ServiceControllers.createService);
 
 router.get("/", ServiceControllers.getAllServices);
 
-// router.get("/:id", ServiceControllers.getSingleService);
+router.get("/:id", ServiceControllers.getSingleService);
 
 router.put("/:id", auth(USER_ROLE.admin), ServiceControllers.updateService);
 
-// router.delete("/:id", auth(USER_ROLE.admin), ServiceControllers.deleteService);
+router.delete("/:id", auth(USER_ROLE.admin), ServiceControllers.deleteService);
 
 router.post("/slots", auth(USER_ROLE.admin), SlotControllers.createSlot);
 
